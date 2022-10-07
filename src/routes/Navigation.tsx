b0224a45dc072a625459bab1654aafa24ff935d8
Diego Cabanillas <Elton.Lara@spsa.pe>
2022-10-07 11:49:20 -0500

    Implementando lazy load básico

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,49 +1,49 @@
+import { Suspense } from 'react';
 import { BrowserRouter, Routes, Route, NavLink, Navigate } from 'react-router-dom';
 import logo from '../logo.svg';
+import { routes } from './routes';
 
 export const Navigation = () => {
   return (
-    <>
+    <Suspense fallback={<span> loading ... </span>}>
       <BrowserRouter>
         <div className="main-layout">
           <nav>
             <img src={logo} alt="React Logo" />
             <ul>
-              <li>
-                <NavLink 
-                  to="/home" 
-                  className={({ isActive }) => isActive ? "nav-active" : ""}
-                > 
-                  Home 
-                </NavLink>
-              </li>
-              <li>
-                <NavLink 
-                  to="/about"
-                  className={({ isActive }) => isActive ? "nav-active" : ""}
-                > 
-                  About 
-                </NavLink>
-              </li>
-              <li>
-                <NavLink 
-                  to="/users"
-                  className={({ isActive }) => isActive ? "nav-active" : ""}
-                > 
-                  Users 
-                </NavLink>
-              </li>
+              { 
+                routes.map(route => {
+                  return (
+                    <li key={route.path}>
+                      <NavLink 
+                        to={route.to}
+                        className={({ isActive }) => isActive ? "nav-active" : ""}
+                      >
+                        { route.name }
+                      </NavLink>
+                    </li>
+                  )
+                }) 
+              }
             </ul>
           </nav>
 
           <Routes>
-            <Route path="about" element={<h1> About Page </h1>}/>
-            <Route path="users" element={<h1> Users Page </h1>}/>
-            <Route path="home" element={<h1> Home Page </h1>}/>
-            <Route path="/*" element={<Navigate to="/home" replace/>}/>
+            {
+              routes.map( route => {
+                return (
+                  <Route
+                    key={route.to}
+                    path={route.path} 
+                    element={<route.Component />} 
+                  />
+                )
+              })
+            }
+            <Route path="/*" element={<Navigate to={routes[0].to} replace/>}/>
           </Routes>
         </div>
       </BrowserRouter>
-    </>
+    </Suspense>
   )
 }
